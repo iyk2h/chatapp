0d1ea9e6f16710c7330d5077102a9e7934e465cb
youngkyunIm <yee0230@naver.com>
2022-10-16 17:03:47 +0900

    feat : add cross orgin

--- src/main/java/com/beamo/chat/ChatRepository.java
+++ src/main/java/com/beamo/chat/ChatRepository.java
@@ -10,5 +10,4 @@ public interface ChatRepository extends ReactiveMongoRepository<Chat,String> {
     @Tailable //커서 계속 유지
     @Query("{'sender':?0,'receiver':?1}")
     Flux<Chat> mFindBySender(String sender, String receiver); // response 유지하면서 데이터 계속 보내기
-
 }
