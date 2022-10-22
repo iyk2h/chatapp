03672243e0015e36762eedb70417a9a90cb628f2
youngkyunIm <yee0230@naver.com>
2022-10-22 21:19:09 +0900

    feat : set Cors

--- src/main/java/com/beamo/chat/chat/ChatController.java
+++ src/main/java/com/beamo/chat/chat/ChatController.java
@@ -14,21 +14,18 @@ import java.time.LocalDateTime;
 public class ChatController {
     private final ChatRepository chatRepository;
 
-    @CrossOrigin
     @GetMapping(value = "/sender/{sender}/receiver/{receiver}",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) {
         return chatRepository.mFindBySender(sender,receiver)
                 .subscribeOn(Schedulers.boundedElastic());
     }
 
-    @CrossOrigin
     @GetMapping(value = "/chat/roomNum/{roomNum}",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Chat> findByRoomNum(@PathVariable Integer roomNum) {
         return chatRepository.mFindByRoomNum(roomNum)
                 .subscribeOn(Schedulers.boundedElastic());
     }
 
-    @CrossOrigin
     @PostMapping("/chat")
     public Mono<Chat> setMsg(@RequestBody Chat chat){
         chat.setCreatedAt(LocalDateTime.now());
